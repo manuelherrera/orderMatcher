7823829d49aaae3a338c8367e3571373aa129e70
manuelherrera <manuelherrera@192.168.0.4>
2014-05-21 23:18:24 -0600

    updating code

--- OrderMatcher/src/com/ordermatcher/service/rules/ItemModelComparator.java
+++ OrderMatcher/src/com/ordermatcher/service/rules/ItemModelComparator.java
@@ -6,7 +6,7 @@ public class ItemModelComparator implements Comparator<SortedItem> {
 
 	@Override
 	public int compare(SortedItem item1, SortedItem item2) {
-		return item1.getItem().compareTo(item2.getItem());
+		return item1.getId().compareTo(item2.getId());
 	}
 
 }
