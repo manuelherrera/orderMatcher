d6926286d72e7aaa1b7dcfe32d06dd02d8e10de5
Jose A Aleman Rojas <josalero@gmail.com>
2014-05-22 19:16:39 -0600

    Adding an exit command handler

--- OrderMatcher/src/com/ordermatcher/presentation/OrderMatcher.java
+++ OrderMatcher/src/com/ordermatcher/presentation/OrderMatcher.java
@@ -16,9 +16,8 @@ public class OrderMatcher {
 		int price = 0;
 		
 		Scanner inputCode = new Scanner(System.in);
-		System.out.print("\nPlease enter a data (BUY/SELL/PRINT) (amount)(@)(price), 'SELL 100@10':\n ");
 		code = inputCode.nextLine().toUpperCase();
-		if (!code.equals("PRINT")){
+		if (!code.equals("PRINT")&&!code.equalsIgnoreCase("exit")){
 			try{	
 				String[] result = code.split("\\s|@");
 				code = result[0];
@@ -34,7 +33,7 @@ public class OrderMatcher {
 				System.out.println("Error: " + "The format is not correct, please try again"); 
 			}
 		}else
-			if (code.equals("PRINT")){
+			if (code.equals("PRINT") || code.equalsIgnoreCase("exit")){
 				item = new OrderItem();
 				item.setCode(code);
 				item.setAmount(0);
@@ -44,12 +43,14 @@ public class OrderMatcher {
 		return item;
 	}	
 		
-	private void findMatch(OrderItem item){
+	private String findMatch(OrderItem item){
 		
-		if (!item.equals(null)){
+		if (!item.equals(null) && !item.getCode().equalsIgnoreCase("exit")){
 			orderMatcherService = new OrderMatcherService();
 			System.out.print(orderMatcherService.computeCommand(item));		
 		}
+		
+		return item.getCode().toLowerCase();
 	}
 	
 	public static void main(String[] args){
@@ -57,18 +58,15 @@ public class OrderMatcher {
 		OrderMatcher orderMatcher = new OrderMatcher();
 		String answer = "";
 		Scanner sc = null;
+		System.out.print("\nPlease enter a data (BUY/SELL/PRINT) (amount)(@)(price), 'SELL 100@10':\nType 'exit' to get out of the application\n");
 		do{			
 			try {
-				orderMatcher.findMatch(orderMatcher.gettingInputValues());
+				answer = orderMatcher.findMatch(orderMatcher.gettingInputValues());
 			}catch (Exception e) {
 				System.out.println("Error: " + "The format is not correct, format valid 'SELL 100@10'"); 
 			}
-			System.out.println("\n");
-			sc = new Scanner (System.in);
-			System.out.println("Do you want to exit?, (yes):\n ");
-			answer = sc.nextLine().toLowerCase();
 		}
-		while (!answer.contentEquals("yes"));
+		while (!answer.contentEquals("exit"));
 		if (answer.contentEquals("yes")){
 			System.out.println("See you later...!!!, next time");
 		}
